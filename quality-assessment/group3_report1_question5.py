208a81bd6dd8015bc4047310e99d372ee81518b1
Michael J. Curry <currymj@fastmail.fm>
2015-10-28 13:27:51 -0400

    added code to compute median for q5

--- quality-assessment/group3_report1_question5.py
+++ quality-assessment/group3_report1_question5.py
@@ -26,21 +26,23 @@ def ascii_to_quality(c, maxscore):
 if __name__ == '__main__':
     total = 0
     count = 0
+    all_values = []
     with open(sys.argv[1]) as f:
         for r in grouper(f, 4, ""):
             if re.search('twodirections', r[0]):
                 qualscores = [ascii_to_quality(c, 30) for c in r[3]]
+                all_values.extend(qualscores)
                 count += len(qualscores)
                 total += math.fsum(qualscores)
     average = total / count
     print "Number of bases: " + str(count)
     print "Average base-calling quality: " + str(average)
-    t = 0
-    with open(sys.argv[1]) as f:
-        for r in grouper(f, 4, ""):
-            if re.search('twodirections', r[0]):
-                quals = [math.pow(ascii_to_quality(c, 30)-average, 2)
-                         for c in r[3]]
-                t += math.fsum(quals)
+    t = math.fsum([math.pow(q - average, 2) for q in all_values])
     sd = math.sqrt(t / (count - 1))
     print "Base-calling standard deviation: " + str(sd)
+    ordered = sorted(all_values)
+    if count % 2 == 0:
+        med = (ordered[(count - 1) / 2] + ordered[(count - 1) / 2 + 1])/2.0
+    else:
+        med = ordered[(count - 1) / 2]
+    print "Median: " + str(med)
