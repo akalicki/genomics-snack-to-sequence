0dbadefafb8e8f10ef16e682073c9a7a347e523e
Alex Kalicki <alexander.kalicki@gmail.com>
2015-11-03 14:41:46 -0500

    add app fasta file selection logic

---
+++ app/js/main.js
@@ -0,0 +1,36 @@
+if (!window.File || !window.FileReader) {
+    throw new Error("The File APIs are not fully supported in this browser.")
+}
+
+function handleDragOver(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+}
+
+function handleFileSelect(e, is_dropzone) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    var reader = new FileReader();
+    reader.onload = (e) => sendFastARequest(e.target.result);
+
+    var files = is_dropzone ? e.dataTransfer.files : e.target.files;
+    for (var i = 0, f; f = files[i]; i++) {
+        reader.readAsText(f);
+    }
+}
+
+function sendFastARequest(fasta) {
+    console.log(fasta);
+}
+
+document.getElementById('files').addEventListener(
+    'change',
+    (e) => handleFileSelect(e, false),
+    false
+);
+
+var dropzone = document.getElementById('dropzone');
+dropzone.addEventListener('dragover', handleDragOver, false);
+dropzone.addEventListener('drop', (e) => handleFileSelect(e, true), false);
